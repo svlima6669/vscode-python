bd33869a22cc6ad47cbb70597a7048e8df50a0e9
Jim Griesmer <jimgries@microsoft.com>
2019-11-20 17:16:52 -0800

    Update to latest analysis and fix gather icon

--- src/datascience-ui/interactive-common/redux/reducers/helpers.ts
+++ src/datascience-ui/interactive-common/redux/reducers/helpers.ts
@@ -71,6 +71,7 @@ export namespace Helpers {
             // and the user has updated the cell text since then.
             newVMs[index] = {
                 ...newVMs[index],
+                hasBeenRun: true,
                 cell: {
                     ...newVMs[index].cell,
                     state: arg.payload.state,
