99c68de5a1238c6c79571c86ba7ffd6b9d75b0ad
Rich Chiodo <rchiodo@microsoft.com>
2020-01-28 14:39:50 -0800

    Fix problem Jim found with jupyterServerFactory not actually being a singleton

--- src/test/datascience/dataScienceIocContainer.ts
+++ src/test/datascience/dataScienceIocContainer.ts
@@ -417,7 +417,7 @@ export class DataScienceIocContainer extends UnitTestIocContainer {
         this.serviceManager.add<INotebookExporter>(INotebookExporter, JupyterExporter);
         this.serviceManager.addSingleton<ILiveShareApi>(ILiveShareApi, MockLiveShareApi);
         this.serviceManager.addSingleton<IExtensions>(IExtensions, MockExtensions);
-        this.serviceManager.add<INotebookServer>(INotebookServer, JupyterServerFactory);
+        this.serviceManager.addSingleton<INotebookServer>(INotebookServer, JupyterServerFactory);
         this.serviceManager.add<IJupyterCommandFactory>(IJupyterCommandFactory, JupyterCommandFactory);
         this.serviceManager.addSingleton<IThemeFinder>(IThemeFinder, ThemeFinder);
         this.serviceManager.addSingleton<ICodeCssGenerator>(ICodeCssGenerator, CodeCssGenerator);
